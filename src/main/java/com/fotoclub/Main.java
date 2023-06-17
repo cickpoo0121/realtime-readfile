9f781671b4560d6896524abfb02915cd2512666d
Tee <cickpoo0121@gmail.com>
2023-06-17 18:13:22 +0700

    init project

---
+++ src/main/java/com/fotoclub/Main.java
@@ -0,0 +1,49 @@
+package com.fotoclub;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program starting...");
+
+        BufferedReader bufferedReader = null;
+        String currentLine = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        // file path can be absolute path
+        String path = "../main/resources/test.txt";
+
+        try {
+            // sample load json string to map
+            String str = "{\r\n  \"userId\": 1,\r\n  \"id\": 1,\r\n  \"title\": \"delectus aut autem\",\r\n  \"completed\": false\r\n}";
+            Map myMap = objectMapper.readValue(str, Map.class);
+            System.out.println(myMap);
+
+            // open read file
+            bufferedReader = new BufferedReader(new FileReader(path));
+            while (true) {
+                if ((currentLine = bufferedReader.readLine()) != null) {
+                    System.out.println(currentLine);
+                    continue;
+                }
+
+                // delay 1s for pending read new line
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+
+            // close read file
+            bufferedReader.close();
+        } catch (Exception err) {
+            System.out.println("Error:" + err);
+        }
+    }
+}
---
+++ src/main/java/com/fotoclub/script/realtime-read-file.sh
@@ -0,0 +1,3 @@
+#javac main.java.com.fotoclub.Main.java
+#jar -cvf main.java.com.fotoclub.Main.jar main.java.com.fotoclub.Main.class
+java -cp F:\Freelance\fotoclubProject\POC\realtime-read-file\target\realtime-read-file-1.0.0-jar-with-dependencies.jar Main
